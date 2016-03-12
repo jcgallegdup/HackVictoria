b13b04cbebaee199a1bc38d1a634735ce41ad6fd
Juan Carlos Gallegos <jcgallegdup@gmail.com>
2016-03-12 12:23:42 -0800

    App set up

---
+++ app/route.js
@@ -0,0 +1,10 @@
+// configure our routes
+myApp.config(function($routeProvider) {
+    $routeProvider
+
+        // route for the home page
+        .when('/', {
+            templateUrl : 'pages/home.html',
+            controller  : 'homeController'
+        })
+});
