64d77740015e965caae8da59aa2c634fcf8c0f83
Ali Siddiqui <alinsiddiqui@gmail.com>
2016-03-12 15:26:58 -0800

    Added colors

--- app/app.module.js
+++ app/app.module.js
@@ -1,1 +1,9 @@
 var myApp = angular.module('myApp', ['ngRoute', 'ngMaterial']);
+
+myApp.config(function($mdThemingProvider) {
+    $mdThemingProvider.theme('default')
+        .primaryPalette('light-green', {
+            'default': '500'
+        })
+        .accentPalette('orange');
+});
