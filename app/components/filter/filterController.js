136a0031540fac346f08c2e87220aab3f9ab4693
Ali Siddiqui <alinsiddiqui@gmail.com>
2016-03-12 20:53:55 -0800

    Added filter page

--- app/components/filter/filterController.js
+++ app/components/filter/filterController.js
@@ -1,3 +1,7 @@
 myApp.controller('filterController', function($scope, $mdDialog, $http, $window, $q, $mdToast, $location) {
-  
+  $scope.types = [
+    "Standard",
+    "Handicap",
+    "Small Car"
+  ];
 });
