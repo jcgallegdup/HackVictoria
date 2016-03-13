36bf558c0d58ecf74e683fa35341b2e2ee206a32
Spenca <asvw8@yahoo.ca>
2016-03-12 21:03:35 -0800

    Added timeLeft

--- app/components/timeLeft/timeLeftController.js
+++ app/components/timeLeft/timeLeftController.js
@@ -1,3 +1,5 @@
 myApp.controller('timeLeftController', function($scope, $mdDialog, $http, $window, $q, $mdToast, $location) {
-  
+  var self = this,  j= 0, counter = 0;
+  self.activated = true;
+  self.determinateValue = 30; // take a value as input in final version
 });
